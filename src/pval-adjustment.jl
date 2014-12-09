8a5396ac5a2512c0a7dbc420eb4d84a59df3ca69
Julian Gehring <julian.gehring@gmail.com>
2014-12-09 13:09:44 +0100

    pval adjustment: basic methods

---
+++ src/pval-adjustment.jl
@@ -0,0 +1,65 @@
+## p-value adjustment methods ##
+
+function bonferroni{T<:FloatingPoint}(pValues::Vector{T})
+    return min(pValues * length(pValues), 1)
+end
+
+
+function benjamini_hochberg{T<:FloatingPoint}(pValues::Vector{T})
+    n = length(pValues)
+    if n <= 1
+        return pValues
+    end
+    sortedIndexes, originalOrder = reorder(pValues)
+    sortedPValues = pValues[sortedIndexes]
+    stepup!(sortedPValues, bejamini_hochberg_multiplier, n)
+    min(sortedPValues[originalOrder], 1)
+end
+
+bejamini_hochberg_multiplier(i::Int, n::Int) = n/(n-i)
+
+
+function holm{T<:FloatingPoint}(pValues::Vector{T})
+    n = length(pValues)
+    if n <= 1
+        return pValues
+    end
+    sortedIndexes, originalOrder = reorder(pValues)
+    sortedPValues = pValues[sortedIndexes]
+    stepdown!(sortedPValues, holm_multiplier, n)
+    min(sortedPValues[originalOrder], 1)
+end
+
+holm_multiplier(i::Int, n::Int) = (n-i+1)
+
+
+function benjamini_yekutieli{T<:FloatingPoint}(pValues::Vector{T})
+    n = length(pValues)
+    if n <= 1
+        return pValues
+    end
+    sortedIndexes, originalOrder = reorder(pValues)
+    sortedPValues = pValues[sortedIndexes]
+    c = sum([1/n for i in 1:n])
+    stepup!(sortedPValues, benjamini_yekutieli_multiplier, n, c)
+    min(sortedPValues[originalOrder], 1)
+end
+
+function bejamini_yekutieli_multiplier(i::Int, n::Int)
+    c = sum([1/i for i in 1:n])
+    return ((n*c)/(n-i))
+end
+
+
+function hochberg{T<:FloatingPoint}(pValues::Vector{T})
+    n = length(pValues)
+    if n <= 1
+        return pValues
+    end
+    sortedIndexes, originalOrder = reorder(pValues)
+    sortedPValues = pValues[sortedIndexes]
+    stepup!(sortedPValues, hochberg_multiplier, n)
+    min(sortedPValues[originalOrder], 1)
+end
+
+hochberg_multiplier(i::Int, n::Int) = (i+1)
