925b4b50cbd4b81498fd3f291a08a07edb772670
Julian Gehring <julian.gehring@gmail.com>
2014-12-09 13:09:19 +0100

    qvalue: storey with given pi0

---
+++ src/qvalue.jl
@@ -0,0 +1,20 @@
+## qValues ##
+using StatsBase
+
+## https://github.com/StoreyLab/qvalue/blob/master/R/qvalue.R
+function qValues{T<:FloatingPoint}(pValues::Vector{T}, pi0::T, pfdr::Bool = false)
+    n = length(pValues)
+    u = sortperm(pValues)
+    v = competerank(pValues) ## ties with 'min'
+    ##v = rank(p, ties.method="max") 
+    if pfdr
+        qvals = (pi0 .* n .* pValues) ./ (v .* (1 - (1 - pValues) .^ n))
+    else
+        qvals = (pi0 .* n .* pValues) ./ v
+    end
+    qvals[u[n]] = min(qvals[u[n]], 1)
+    for i in (n - 1):-1:1
+        qvals[u[i]] = min(qvals[u[i]], qvals[u[i + 1]])
+    end
+    return qvals
+end
