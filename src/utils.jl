7dd22e871c7bff4b9be4a344aabc240353380f90
Julian Gehring <julian.gehring@gmail.com>
2014-12-09 13:10:07 +0100

    utils: streamline functions

--- src/utils.jl
+++ src/utils.jl
@@ -1,31 +1,40 @@
 ## utility functions ##
 
-function stepup!(sortedPValues, multiplier)
-    for i in 0:(n - 1)
-        if i == 0
-            sortedPValues[n-i] *= multiplier
-        else
-            sortedPValues[n-i] = min(sortedPValues[n-i+1], sortedPValues[n-i] * multiplier)
-        end
+function stepup!{T<:FloatingPoint}(sortedPValues::Vector{T}, multiplier::Function, n::Integer = length(sortedPValues))
+    sortedPValues[n] *= multiplier(0, n)
+    for i in 1:(n-1)
+        sortedPValues[n-i] = min(sortedPValues[n-i+1], sortedPValues[n-i] * multiplier(i, n))
     end
     return sortedPValues
 end
 
 
-function stepdown!(sortedPValues, multiplier)
-    for i in 1:n
-      if i == 1
-        sortedPValues[i] *= multiplier
-      else
-        sortedPValues[i] = max(sortedPValues[i-1], sortedPValues[i] * multiplier)
-      end
+function stepdown!{T<:FloatingPoint}(sortedPValues::Vector{T}, multiplier::Function, n::Integer = length(sortedPValues))
+    sortedPValues[1] *= multiplier(1, n)
+    for i in 2:n
+        sortedPValues[i] = max(sortedPValues[i-1], sortedPValues[i] * multiplier(i, n))
     end
     return sortedPValues
 end
 
 
-function reorder(values)
+function reorder{T<:Number}(values::Vector{T})
     newOrder = sortperm(values)
     oldOrder = sortperm(newOrder)
     return newOrder, oldOrder
+end
+
+
+function validPValues{T<:FloatingPoint}(x::Vector{T})
+    ex = extrema(x)
+    if ex[1] .< 0.0 || ex[2] .> 1.0
+        throw DomainError()
+    end
+    return res
+end
+
+function checkPValues{T<:FloatingPoint}(pValues::Vector{T})
+    if any((pValues .< 0.0) | (pValues .> 1.0))
+        throw DomainError()
+    end
 end
