ccde22780fb49f3fafa528a48fb76398d7084dc9
Julian Gehring <julian.gehring@gmail.com>
2015-06-04 14:16:15 +0200

    Tests for pi0: bootstrap and lsl
    The tests for lsl can only be preliminary, since it only compares the
    vectorized with the devectorized implementation for now.

--- test/test-pi0.jl
+++ test/test-pi0.jl
@@ -3,6 +3,7 @@ module Test_pi0
 
 using MultipleTesting
 using Base.Test
+using StatsBase
 
 ## test case: deterministic
 p0 = [0.01:0.01:1; ]
@@ -11,6 +12,7 @@ p = [p0; p1] ## unsorted
 pi0 = length(p0) / length(p)
 
 lambdas = [0.05:0.05:0.95; ]
+lambdas_unsort = sample(lambdas, length(lambdas), replace = false)
 
 ## storey_pi0 ##
 println(" ** ", "storey_pi0")
@@ -25,5 +27,17 @@ println(" ** ", "bootstrap_pi0")
 @test_approx_eq bootstrap_pi0(p, lambdas) 0.6
 @test_approx_eq bootstrap_pi0(p0, lambdas) 1.0
 @test_approx_eq bootstrap_pi0(p1, lambdas) 0.15
+## unsorted lambdas
+@test_approx_eq bootstrap_pi0(p, lambdas_unsort) 0.6
+## with default 'lambdas'
+@test_approx_eq bootstrap_pi0(p) 0.6
+@test_approx_eq bootstrap_pi0(p0) 1.0
+@test_approx_eq bootstrap_pi0(p1) 0.15
+
+## lsl_pi0 ##
+println(" ** ", "lsl_pi0", ": Insufficient tests!")
+@test_approx_eq lsl_pi0(p) MultipleTesting.lsl_pi0_vec(p)
+@test_approx_eq lsl_pi0(p0) MultipleTesting.lsl_pi0_vec(p0)
+@test_approx_eq lsl_pi0(p1) MultipleTesting.lsl_pi0_vec(p1)
 
 end
