192e6d2a3f5f0a5ac988dcd3cfd6d99fb5b84746
Julian Gehring <julian.gehring@gmail.com>
2017-01-08 21:00:17 +0000

    Harmonise code formatting

--- test/test-grenander.jl
+++ test/test-grenander.jl
@@ -38,12 +38,12 @@ using Base.Test
     @testset "Isotonic regression" begin
 
         #pooled adjacent violators example page 10 robertson
-        isotonicregression = MultipleTesting.isotonicregression
-        @test isotonicregression([22.5; 23.333; 20.833; 24.25], [3.0;3.0;3.0;2.0]) ≈ [22.222; 22.222; 22.222; 24.25]
+        isotonic_regression_reference = MultipleTesting.isotonic_regression_reference
+        @test isotonic_regression_reference([22.5; 23.333; 20.833; 24.25], [3.0;3.0;3.0;2.0]) ≈ [22.222; 22.222; 22.222; 24.25]
 
         #if input already ordered, then output should be the same
-        @test isotonicregression([1.0; 2.0; 3.0]) ≈ [1.0; 2.0; 3.0]
-        @test isotonicregression([1., 41., 51., 1., 2., 5., 24.], [1., 2., 3., 4., 5., 6., 7.]) ≈ [1.0, 13.95, 13.95, 13.95, 13.95, 13.95, 24]
+        @test isotonic_regression_reference([1.0; 2.0; 3.0]) ≈ [1.0; 2.0; 3.0]
+        @test isotonic_regression_reference([1., 41., 51., 1., 2., 5., 24.], [1., 2., 3., 4., 5., 6., 7.]) ≈ [1.0, 13.95, 13.95, 13.95, 13.95, 13.95, 24.]
 
         # single value or empty vector remains unchanged
         r = rand(1)
