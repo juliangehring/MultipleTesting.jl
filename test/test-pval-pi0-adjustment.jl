295847483d77d771b07b0d270031144132066034
Julian Gehring <julian.gehring@gmail.com>
2017-03-04 17:18:43 +0000

    Update test cases and reference values for `qValues`

--- test/test-pval-pi0-adjustment.jl
+++ test/test-pval-pi0-adjustment.jl
@@ -9,9 +9,6 @@ using Base.Test
     pval1 = [0.0, 0.0001, 0.001, 0.01, 0.05, 0.1, 0.2, 0.4, 0.75, 1.0]
     pi0 = 0.4
 
-    ## compared against:
-    ## qvalue::qvalue(p, pi0, pfdr = TRUE, pi0.method = "bootstrap")
-
     @test_throws MethodError adjust()
     @test_throws MethodError adjust(pval1)
 
@@ -37,12 +34,12 @@ using Base.Test
         @test adjust(pval, t(pi0)) == pval .* pi0
 
         ## compare with reference values
-        @test isapprox( adjust(pval1, t(pi0)), ref, atol = 1e-6 )
+        @test isapprox( adjust(pval1, t(pi0)), ref, atol = 1e-8 )
 
         ## BH Adaptive same as BH for π0 missing or 1
-        @test isapprox( adjust(pval1, t(1.0)), ref0, atol = 1e-6 )
-        @test isapprox( adjust(pval1, t()), ref0, atol = 1e-6 )
-        @test isapprox( adjust(pval1, t(0.0)), zeros(ref0), atol = 1e-6 )
+        @test isapprox( adjust(pval1, t(1.0)), ref0, atol = 1e-8 )
+        @test isapprox( adjust(pval1, t()), ref0, atol = 1e-8 )
+        @test isapprox( adjust(pval1, t(0.0)), zeros(ref0), atol = 1e-8 )
 
         # adaptive with pi0 estimator == oracle with estimated pi0
         @test adjust(pval1, t(Storey())) == adjust(pval1, t(estimate_pi0(pval1, Storey())))
@@ -56,7 +53,9 @@ using Base.Test
 
         m = qValues
 
-        ref = [0.0, 0.0002, 0.00133333, 0.01, 0.04, 0.0666667, 0.114286, 0.2, 0.333333, 0.4]
+        # reference values taken from `qvalue` package, v2.6.0
+        # ```qvalue::qvalue(pval1, pi0, pfdr = FALSE, pi0.method = "bootstrap")$qvalues```
+        ref = [0.0, 0.0002, 0.001333333, 0.01, 0.04, 0.066666667, 0.114285714, 0.2, 0.333333333, 0.4]
 
         @test_throws MethodError m()
 
@@ -74,8 +73,8 @@ using Base.Test
         @test m(pval, pi0) == pval .* pi0
 
         ## compare with reference values
-        @test isapprox( m(pval1, pi0), ref, atol = 1e-6 )
-        @test isapprox( m(pval1, pi0, false), ref, atol = 1e-6 )
+        @test isapprox( m(pval1, pi0), ref, atol = 1e-8 )
+        @test isapprox( m(pval1, pi0, false), ref, atol = 1e-8 )
 
     end
 
@@ -84,7 +83,9 @@ using Base.Test
 
         m = qValues
 
-        ref = [0.099685, 0.099685, 0.099685, 0.099685, 0.099685, 0.102356, 0.128033, 0.201217, 0.333333, 0.4]
+        # reference values taken from `qvalue` package, v2.6.0
+        # ```qvalue::qvalue(pval1, pi0, pfdr = TRUE, pi0.method = "bootstrap")$qvalues```
+        ref = [NaN, 0.09968523, 0.09968523, 0.09968523, 0.09968523, 0.10235600, 0.12803317, 0.20121668, 0.33333365, 0.4]
 
         @test_throws MethodError m()
 
@@ -102,7 +103,10 @@ using Base.Test
         @test m(pval, pi0) == pval .* pi0
 
         ## compare with reference values
-        @test isapprox( m(pval1, pi0, true), ref, atol = 1e-5 )
+        # `isapprox` cannot compare NaNs in julia 0.5
+        # TODO use `isapprox(nans = true)` in julia 0.6
+        idx = !isnan.(ref)
+        @test isapprox( m(pval1, pi0, true)[idx], ref[idx], atol = 1e-8 )
 
     end
 
