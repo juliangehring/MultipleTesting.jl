270e859955a0f3decbef232d2a82d661dd091be4
Julian Gehring <juliangehring@users.noreply.github.com>
2018-04-05 19:19:02 +0100

    Optimise and simplify qvalue estimation

--- src/qvalue.jl
+++ src/qvalue.jl
@@ -4,16 +4,17 @@ function qValues{T<:AbstractFloat}(pValues::AbstractVector{T}, pi0::AbstractFloa
     valid_pvalues(pValues)
     valid_pvalues([pi0])
     n = length(pValues)
-    u = sortperm(pValues)
-    v = competerank(pValues) # ties with 'min'
-    if pfdr
-        qvals = (pi0 .* n .* pValues) ./ (v .* (1 - (1 - pValues) .^ n))
-    else
-        qvals = (pi0 .* n .* pValues) ./ v
+    sortedIndex = sortperm(pValues)
+    qValues = zeros(pValues)
+    q = T(Inf)
+    for i in n:-1:1
+        idx = sortedIndex[i]
+        if pfdr
+            q = min(pValues[idx] * n / (i * (1 - (1 - pValues[idx]) ^ n)), q)
+        else
+            q = min(pValues[idx] * n / i, q)
+        end
+        qValues[idx] = pi0 * q
     end
-    qvals[u[n]] = min.(qvals[u[n]], 1)
-    for i in (n - 1):-1:1
-        qvals[u[i]] = min.(qvals[u[i]], qvals[u[i + 1]])
-    end
-    return qvals
+    return qValues
 end
