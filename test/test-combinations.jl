0dfd0395a272cedfc5c9a09627c85036aed5d0c4
Julian Gehring <juliangehring@users.noreply.github.com>
2020-05-16 18:40:19 +0100

    Format tests

--- test/test-combinations.jl
+++ test/test-combinations.jl
@@ -9,25 +9,31 @@ using Test
 @testset "p-Value combinations" begin
 
     p1 = [0.01, 0.05, 0.2, 0.8]
-    ref1 = Dict(FisherCombination   => 0.01558752, # `metap::sumlog(p)`
+    ref1 = Dict(
+        FisherCombination   => 0.01558752, # `metap::sumlog(p)`
         LogitCombination    => 0.020031,   # `metap::logitp(p)`
         StoufferCombination => 0.02353884, # `metap::sumz(p)`
         TippettCombination  => 0.03940399, # `gmeta::Cpvaluecombine(p, "tippett")`
-        SimesCombination    => 0.04)
+        SimesCombination    => 0.04
+    )
 
     p2 = [0.0001, 0.001, 0.01, 0.05, 0.1, 0.2, 0.4, 0.75]
-    ref2 = Dict(FisherCombination   => 1.285885e-06, # `metap::sumlog(p)`
+    ref2 = Dict(
+        FisherCombination   => 1.285885e-06, # `metap::sumlog(p)`
         LogitCombination    => 1.801132e-06, # `metap::logitp(p)`
         StoufferCombination => 5.092143e-06, # `metap::sumz(p)`
         TippettCombination  => 0.0007997201, # `gmeta::Cpvaluecombine(p, "tippett")`
-        SimesCombination    => 0.0008)
+        SimesCombination    => 0.0008
+    )
 
     p3 = copy(p1)
     w3 = [0.1, 0.2, 0.3, 0.4] .* 2
     ref3 = Dict(StoufferCombination => 0.1916892)
 
-    p_adjust_combinations = Dict(Sidak             => TippettCombination,
-        BenjaminiHochberg => SimesCombination)
+    p_adjust_combinations = Dict(
+        Sidak             => TippettCombination,
+        BenjaminiHochberg => SimesCombination
+    )
 
     p_single = rand(1)
 
