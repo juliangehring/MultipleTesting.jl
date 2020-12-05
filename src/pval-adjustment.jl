dd02c47b9806ba4ad00fab2b98198f96b58d6e11
Julian Gehring <juliangehring@users.noreply.github.com>
2020-12-06 00:09:39 +0100

    Update docs deployment (#120)
    * Update docs deployment
    * Update results of doctests

--- src/pval-adjustment.jl
+++ src/pval-adjustment.jl
@@ -285,7 +285,7 @@ julia> pvals = PValues([0.001, 0.01, 0.03, 0.5]);
 julia> adjust(pvals, BenjaminiLiu())
 4-element Array{Float64,1}:
  0.003994003998999962
- 0.022275749999999983
+ 0.022275750000000066
  0.02955000000000002
  0.125
 
